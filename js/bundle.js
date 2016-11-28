ce2a35583a6ab73733582d750ef90dec3489a9df
afioca <afioca2@gmail.com>
2016-11-28 14:46:50 -0500

    refactor Game object to have list of enemyShips instead of 1 enemy ship

--- js/bundle.js
+++ js/bundle.js
@@ -69,16 +69,18 @@
 
 	  // this.spaceship1 = Spaceship.create(200, 200, "#0F0");
 	  this.spaceship1 = Spaceship.create(0, 0, "#0F0", "/demo-game/img/spaceship.png");
-	  this.spaceship2 = Spaceship.create(50, 100, "#F00");
 
+	  this.enemyShipCount = 2;
 	  this.projectiles = [];
 	  this.explosions = [];
+	  this.enemyShips = [];
 
 	  this.init = function() {
 	    this.spaceship1.draw();
-	    this.spaceship2.draw();
 	    this.stage.addChild(this.spaceship1.getSelf());
-	    this.stage.addChild(this.spaceship2.getSelf());
+
+	    this._createEnemyShips();
+
 	    this.configureTicker();
 
 	    this.stage.addEventListener("click", this._fire.bind(this));
@@ -97,7 +99,8 @@
 	  this.tick = function() {
 	    if (!this.isPaused) {
 	      this._moveSpaceship1();
-	      this._moveSpaceship2();
+	      // this._moveSpaceship2();
+	      this._moveEnemyShips();
 	      this._updateProjectiles();
 	      this._updateExplosions();
 	      this.stage.update();
@@ -111,11 +114,13 @@
 	    }
 	  };
 
-	  this._moveSpaceship2 = function() {
-	    if (this.spaceship2.getLeftBoundry() >= this.width) {
-	      this.spaceship2.moveToX(0 - this.spaceship1.radius);
+	  this._moveEnemyShips = function() {
+	    for (var i = 0; i < this.enemyShipCount; i++) {
+	      if (this.enemyShips[i].getLeftBoundry() >= this.width) {
+	        this.enemyShips[i].moveToX(0 - this.enemyShips[i].radius);
+	      }
+	      this.enemyShips[i].moveSpaces(1, 0);
 	    }
-	    this.spaceship2.moveSpaces(1, 0);
 	  };
 
 	  this._fire = function() {
@@ -145,10 +150,13 @@
 	    for (var i = 0; i < this.projectiles.length; i++) {
 	      var projectile = this.projectiles[i];
 	      projectile.move();
-	      if(this._collidesWithShip2(projectile)) {
-	        // projectile.isExpired = true;
-	        // var explosion = new Explostion(this.spaceship2.getCurrentX(), this.spaceship2.getBottomBoundry());
-	        // explosion.draw()
+	      this._handleEnemyCollisions(projectile);
+	    }
+	  };
+
+	  this._handleEnemyCollisions = function(projectile) {
+	    for (var i = 0; i < this.enemyShipCount; i++) {
+	      if(this._collidesWithEnemyShip(this.enemyShips[i], projectile)) {
 	        var explosion = projectile.explode();
 	        this.stage.addChild(explosion.getSelf());
 	        this.explosions.push(explosion);
@@ -156,10 +164,10 @@
 	    }
 	  };
 
-	  this._collidesWithShip2 = function(projectile) {
-	    return (projectile.getTopBoundry() <= this.spaceship2.getBottomBoundry() &&
-	    (projectile.getCurrentX() >= this.spaceship2.getLeftBoundry() &&
-	    projectile.getCurrentX() <= this.spaceship2.getRightBoundry()));
+	  this._collidesWithEnemyShip = function(ship, projectile) {
+	    return (projectile.getTopBoundry() <= ship.getBottomBoundry() &&
+	    (projectile.getCurrentX() >= ship.getLeftBoundry() &&
+	    projectile.getCurrentX() <= ship.getRightBoundry()));
 	  };
 
 	  this._updateExplosions = function() {
@@ -169,6 +177,19 @@
 	        this.stage.removeChild(this.explosions[i].getSelf());
 	        this.explosions.splice(i, 1);
 	      }
+	    }
+	  };
+
+	  this._createEnemyShips = function() {
+	    var startingX = 50;
+	    var startingY = 50;
+	    for (var i = 0; i < this.enemyShipCount; i++) {
+	      var enemyShip = Spaceship.create(startingX, startingY, "#F00");
+	      enemyShip.draw();
+	      this.stage.addChild(enemyShip.getSelf());
+	      this.enemyShips.push(enemyShip);
+	      startingX = startingX + 100;
+	      startingY = startingY + 100;
 	    }
 	  };
 	}
